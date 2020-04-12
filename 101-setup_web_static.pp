4b2e43a3a13ce6890313110b373bde9be678b275
doubix <adib.grouz@gmail.com>
2020-04-12 21:56:11 +0100

    [update] commands

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,8 +1,10 @@
 # Configures a web server for deployment of web_static.
 
 exec { 'setup server':
-  command => 'sudo apt-get -y update && sudo apt-get -y install nginx;
-              sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/; ; 
+  command => 'sudo apt-get -y update;
+              sudo apt-get -y install nginx;
+              sudo mkdir -p /data/web_static/releases/test/; 
+              sudo mkdir -p /data/web_static/shared/; 
               sudo touch /data/web_static/releases/test/index.html; 
               sudo echo "Hello Holberton School" > /data/web_static/releases/test/index.html;
               sudo ln -sf /data/web_static/releases/test /data/web_static/current;
